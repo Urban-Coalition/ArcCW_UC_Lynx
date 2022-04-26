3ce128e48ee8c76e73de568b985f7e3b9b68bf7b
Fesiug <publicfesiug@outlook.com>
2022-04-26 00:12:10 -0400

    Fuckass

--- lua/weapons/arccw_uc_lynx.lua
+++ lua/weapons/arccw_uc_lynx.lua
@@ -21,8 +21,8 @@ end
 
 SWEP.UseHands = true
 
-SWEP.ViewModel		=	"models/weapons/arccw/c_uc_lynx2.mdl"
-SWEP.WorldModel		=	"models/weapons/arccw/c_uc_lynx2.mdl"
+SWEP.ViewModel		=	"models/weapons/arccw/c_uc_lynx3.mdl"
+SWEP.WorldModel		=	"models/weapons/arccw/c_uc_lynx3.mdl"
 SWEP.ViewModelFOV	=	80
 
 SWEP.MirrorVMWM = true
