0ec39084e45fe2275fcbdea7f9861e983b8c7556
TheOnly8Z <888888zombies@gmail.com>
2022-07-31 00:26:35 -0500

    change 50 beowulf

--- lua/arccw/shared/attachments/uc_lynx_caliber_127.lua
+++ lua/arccw/shared/attachments/uc_lynx_caliber_127.lua
@@ -1,38 +1,47 @@
 att.PrintName = ".50 Beowulf"
 att.Icon = Material("entities/att/acwatt_ud_glock_caliber.png")
-att.Description = "Special duty high-penetrating caliber for penetrating armoured vehicles and aircraft."
+att.Description = "Semi-automatic receiver chambered in .50 Beowulf, a gigantic short-range cartridge with terrible ballistic performance.\nThe rifle is unable to handle the massive pressure at anything more than semi-automatic fire."
 att.Desc_Pros = {
 }
 att.Desc_Cons = {
 }
 att.AutoStats = true
 att.Slot = "uc_lynx_caliber"
-att.SortOrder = 1270
 
-att.Override_Ammo = "ar2"
-att.Mult_ClipSize = 1 / 3
-att.Override_DamageType = DMG_AIRBOAT + DMG_BLAST
+att.SortOrder = 50
+
+att.Override_ClipSize = 7
+att.Override_ClipSize_Priority = 0.5
+att.BaseClipSize = 7
 
 att.Mult_Recoil = 2
 att.Mult_RecoilSide = 2
 
-att.Mult_AccuracyMOA = 0.125
 att.Mult_RPM = 0.5
 att.Mult_Penetration = 3
 
-att.Override_PhysBulletMuzzleVelocity = 550
 att.Mult_Damage = 80 / 40
 att.Mult_DamageMin = 20 / 15
 att.Mult_Range = 0.85
 att.Mult_RangeMin = 0.85
 
+att.Override_PhysBulletMuzzleVelocity = 550
+
+-- yes, .50 beowulf is more of an huge magnum bullet than it is a rifle bullet
+att.Override_Ammo = "357"
+att.Override_Trivia_Calibre = ".50 Beowulf"
+att.Override_ShellModel = "models/weapons/arccw/uc_shells/50beo.mdl"
+att.Override_ShellScale = 1
+att.Override_ShellSounds = ArcCW.PistolShellSoundsTable
+
+att.Override_Firemodes_Priority = 0.5
 att.Override_Firemodes = {
-	{
-		Mode = 1,
-	},
-	{
-		Mode = 0
-	}
+    {
+        Mode = 1,
+    },
+    {
+        Mode = 0
+    }
 }
 
 att.Hook_GetShootSound = function(wep, anim)
