d030be2478b60e05a43e5c0a38c7aa15f0dbc022
Talon141 <zachlaxtonbiz@gmail.com>
2023-01-22 09:44:14 -0600

    ext handguard cosmetic tag

--- lua/arccw/shared/attachments/uc_lynx_hg_long.lua
+++ lua/arccw/shared/attachments/uc_lynx_hg_long.lua
@@ -1,9 +1,8 @@
 att.PrintName = "Extended Handguard"
 att.Icon = Material("entities/att/acwatt_uc_lynx_hg_long.png", "mips smooth")
 att.Description = "Extended M-LOK handguard to stuff a suppressor inside!"
-att.Desc_Pros = {
-}
-att.Desc_Cons = {
+att.Desc_Neutrals = {
+    "uc.cosmetic"
 }
 att.AutoStats = true
 att.Slot = "uc_lynx_handguard"
