6ef24f5b4463d362511ea7e578fc4ae37b22cfdc
Fesiug <publicfesiug@outlook.com>
2023-01-22 19:33:01 -0500

    stock shit

--- lua/weapons/arccw_uc_lynx.lua
+++ lua/weapons/arccw_uc_lynx.lua
@@ -315,10 +315,10 @@ SWEP.Attachments = {
 		Slot = {"go_stock", "uc_lynx_stock"},
 		Bone = "wpn",
 		Offset = {
-			vpos = Vector(0.1, -3.7, 1.1),
+			vpos = Vector(0.0, -3.7, 1.1),
 			vang = Angle(90, -90, -90),
 		},
-		VMScale = Vector(1.1, 1.1, 1.1),
+		VMScale = Vector(1, 1, 1),
 		InstalledEles = {"uc_lynx_stock_none"},
 	},
 	{
